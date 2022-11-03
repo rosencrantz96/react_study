03f738c45dd9b2a4be73f68b8befea346bed8350
rosencrantz96 <topgdvidsyb@naver.com>
2022-11-03 18:33:18 +0900

    up

---
+++ 05.hooks/src/Info.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+class Info extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Info;
