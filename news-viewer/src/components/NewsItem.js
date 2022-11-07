85e4b78dafbc5aa4c2c49710aa7f76e545f1b172
rosencrantz96 <topgdvidsyb@naver.com>
2022-11-07 13:47:46 +0900

    up

---
+++ news-viewer/src/components/NewsItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewsItemBlock = styled.div`
+  display: flex;
+
+  .thumbnail {
+    margin-right: 1rem;
+    img {
+      display: block;
+      width: 160px;
+      height: 100px;
+      object-fit: cover;
+    }
+  }
+
+  .contents {
+    h2 {
+      margin: 0;
+      a {
+        color: black;
+      }
+    }
+    p {
+      margin: 0;
+      line-height: 1.5;
+      margin-top: 0.5rem;
+      white-space: normal;
+    }
+  }
+
+  & + & {
+    margin-top: 3rem;
+  }
+`;
+
+function NewsItem({ article }) {
+  const { title, description, url, urlToImage } = article;
+  return (
+    <NewsItemBlock>
+      {urlToImage && (
+        <div className="thumbnail">
+          {/* rel="noopener noreferrer" 새창으로 띄울때 보안 취약점 방지 및 성능 향상 */}
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={urlToImage} alt="thumbnail" />
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </NewsItemBlock>
+  );
+}
+
+export default NewsItem;
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+function NewsList({ category }) {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const query = category === "all" ? "" : `&category=${category}`;
+          const response = await axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1076b488e7a642b6b1f9950f13d28644`
+          );
+          setArticles(response.data.articles);
+        } catch (e) {}
+        setLoading(false);
+      };
+  
+      fetchData();
+    }, [category]);
+  
+    //대기 중일 때
+    if (loading) {
+      return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+  
+    //articles의 값이 아직 설정이 안됐을 때
+    if (!articles) {
+      return null;
+    }
+  
+    return (
+      <NewsListBlock>
+        {articles.map((article) => (
+          <NewsItem key={article.url} article={article} />
+        ))}
+      </NewsListBlock>
+    );
+  }
+export default NewsList;
